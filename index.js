6d84ae00884a223eb1aeff9a574feb3dc9ee1d83
Amisoz <amizosgame@gmail.com>
2018-06-16 20:41:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+var bot = new Discord.Client();
+var logChannel = '457612965470797825';
+var idBot = '457614601723576330';
+
+bot.on("ready", function(){
+    bot.user.setPresence({ game: { name: 'https://discord.gg/fE5uZXE', type: 0 } });
+})  
+
+bot.on("messageDelete", message => {
+    var sender = message.author;
+    var msg = message.content;
+    
+    if(sender.id == idBot) return;
+
+    /** Logs de message (modération) */
+    var date = new Date();
+    var dateResult = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " à " + date.getHours() + ":" + date.getMinutes();   
+
+    bot.channels.get(logChannel).send([dateResult] + " " + sender.username + " - " + message.channel.name + " : " + msg);
+    
+    /** Fin de log */
+})
+
+bot.login(process.env.TOKEN);
